c3cf6b4cb596c89061249010352bd030f9fbf222
fclef978 <fclef978@icloud.com>
2018-01-03 08:25:24 +0900

    battery追加

--- arduino.py
+++ arduino.py
@@ -108,7 +108,7 @@ class Arduino:
         elif cmd == "velocity":
             ser_data = "RM:4;VL:{0};VR:{1};".format(cmd_data[1], cmd_data[2])
         elif cmd == "battery":
-            ser_data = "RM:5;BA:{0};BB:{1};".format(cmd_data[1], cmd_data[2])
+            ser_data = "RM:10;BA:{0};BB:{1};".format(cmd_data[1], cmd_data[2])
         elif cmd == "reset":
             ser_data = "RM:100;"
         else:

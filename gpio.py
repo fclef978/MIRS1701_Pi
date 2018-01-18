e0fe84838540655db5e6990191cda2b442228ddb
fclef978 <fclef978@icloud.com>
2018-01-18 12:54:04 +0900

    gpioを忘れてた

---
+++ gpio.py
@@ -0,0 +1,55 @@
+from RPi import GPIO
+import time
+
+
+class IO:
+    FRONT = 18
+    LEFT = 25
+    RIGHT = 26
+
+    IN = GPIO.IN
+    OUT = GPIO.OUT
+
+    def __init__(self, port, dir, isPullUp=False):
+        self.port = port
+        self.dir = dir
+        self.isPullUp = isPullUp
+        GPIO.setmode(GPIO.BCM)
+        if self.dir == IO.IN:
+            if self.isPullUp:
+                GPIO.setup(self.port, self.dir, pull_up_down=GPIO.PUD_UP)
+            else:
+                GPIO.setup(self.port, self.dir)
+        elif self.dir == IO.OUT:
+            GPIO.setup(self.port, self.dir)
+
+    def __del__(self):
+        self.off()
+        GPIO.cleanup(self.port)
+
+    def read(self):
+        return GPIO.input(self.port)
+
+    def write(self, state):
+        if self.dir == IO.OUT:
+            GPIO.output(self.port, state)
+            return True
+        else:
+            print("Port {0} is not OUTPUT".format(self.port))
+            return False
+            
+    def on(self):
+        self.write(True)
+            
+    def off(self):
+        self.write(False)
+
+    def inv(self):
+        self.write(not self.read())
+
+
+if __name__ == "__main__":
+    io = IO(12, IO.OUT)
+    io.on()
+    io.off()
+    io.inv()

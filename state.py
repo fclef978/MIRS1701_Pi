4b1bc72a96a668a9414cbc7b40035b5b53f57cc0
704Left <kaler_night@yahoo.co.jp>
2018-01-31 15:12:14 +0900

    stateにwaitを追加

--- state.py
+++ state.py
@@ -1,9 +1,15 @@
 class State:
+    FRONT_INPUT_VALUE = 100
+    BACK_INPUT_VALUE = 900
+    LEFT_INPUT_VALUE = 900
+    RIGHT_INPUT_VALUE = 100
+
 
     def __init__(self, data):
         self.state = "init"
         self.data = data
         self.is_changed = False
+        self.expected = "straight"
 
     def judge(self):
         prev = self.state
@@ -13,18 +19,31 @@ class State:
             self.is_changed = True
 
     def init(self):
-        if self.data.ard["jsY"] < 100:  # 左壁右入力
+        if self.data.ard["jsY"] < 100:
             self.state = "straight"
 
     def straight(self):
         if self.data.uss["sf"] > 150:
-            self.state = "turn"
+            self.state = "wait"
+            self.expected = "turn"
         if self.data.is_left and self.data.ard["jsX"] < 100:  # 左壁右入力
             self.state = "change"
         if not self.data.is_left and self.data.ard["jsX"] > 900:  # 右壁左入力
             self.state = "change"
         if self.data.uss["f"] < 40:
-            self.state = "avoid"
+            self.state = "wait"
+            self.expected = "avoid"
+        if self.data.ard["jsY"] < 900:
+            self.state = "wait"
+            self.expected = "straight"
+
+    def wait(self):
+        if self.expected == "turn":
+            if self.data.is_left and self.data.ard["jsX"] > 900:
+                self.state = "turn"
+        else:
+            self.state = self.expected
+
 
     def avoid(self):
         if self.data.uss["f"] > 40:

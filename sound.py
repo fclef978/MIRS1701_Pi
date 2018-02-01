a5b953d3c2241e1c678c11c9f0aac983d7f15b90
704Left <kaler_night@yahoo.co.jp>
2018-02-01 15:45:16 +0900

    音声通知

--- sound.py
+++ sound.py
@@ -4,14 +4,14 @@ from time import sleep
 
 
 class Sound():
-    
     def __init__(self):
         self.openJtalk = ['open_jtalk']
-        self.dic = ['-x','/var/lib/mecab/dic/open-jtalk/naist-jdic']
-        self.htsVoice=['-m','/usr/share/hts-voice/mei/mei_normal.htsvoice']
-        self.speed=['-r','1.0']
+        self.dic = ['-x', '/var/lib/mecab/dic/open-jtalk/naist-jdic']
+        self.htsVoice = ['-m', '/usr/share/hts-voice/mei/mei_normal.htsvoice']
+        self.speed = ['-r', '1.0']
         self.sp_ssw = IO(IO.SP_SSW, IO.OUT)
-        
+        self.sp = None
+
     def talk(self, text):
         """
         openJtalkに喋らせるメソッドです。
@@ -21,16 +21,21 @@ class Sound():
         return self.say()
 
     def generate_wave(self, text, name='tmp.wav'):
-        outWav = ['-ow', './wav/'+name]
+        outWav = ['-ow', './wav/' + name]
         cmd = self.openJtalk + self.dic + self.htsVoice + self.speed + outWav
-        c = subprocess.Popen(cmd,stdin=subprocess.PIPE)
+        c = subprocess.Popen(cmd, stdin=subprocess.PIPE)
         c.stdin.write(text.encode('utf-8'))
         c.stdin.close()
         c.wait()
 
     def say(self, name='tmp.wav'):
-        aplay = ['aplay','-q','./wav/'+name]
-        return subprocess.Popen(aplay)
+        try:
+            self.sp.terminate()
+        except:
+            pass
+        aplay = ['aplay', '-q', './wav/' + name]
+        self.sp =  subprocess.Popen(aplay)
+        return self.sp
 
     def speak_help(self):
         self.sp_ssw.on()
@@ -38,6 +43,18 @@ class Sound():
         # self.say(name='help.wav')
         self.talk("テスト").wait()
         self.sp_ssw.off()
+
+    def say_init(self):
+        self.say("init.wav")
+
+    def say_dist(self, dist_count):
+        self.say("dist_" + str(dist_count) + ".wav")
+
+    def say_straight(self):
+        self.say("straight.wav")
+
+    def say_turn(self):
+        self.say("turn.wav")
 
     def say_corner(self):
         self.say(name='corner.wav')
@@ -51,6 +68,20 @@ class Sound():
     def say_stop(self):
         self.say(name='stop.wav')
 
+    def say_wait(self, expected):
+        if expected == "turn":
+            self.say(name='corner.wav')
+        if expected == "avoid":
+            self.say(name='obstacle.wav')
+
+
 if __name__ == "__main__":
     s = Sound()
     s.speak_help()
+    s.generate_wave("ちょくしんをかいしするにはまえをにゅうりょくしてください", "init.wav")
+    s.generate_wave("ちょくしんをさいかいするにはまえをにゅうりょくしてください", "straight_wait.wav")
+    s.generate_wave("めーとる、けいかしました", "dist.wav")
+    s.generate_wave("ちょくしんします", "straight.wav")
+    s.generate_wave("まがります", "turn.wav")
+    for i in range(1, 4, 5):
+        s.generate_wave(str(i) + "めーとる、すすみました", "dist_" + str(i) + ".wav")

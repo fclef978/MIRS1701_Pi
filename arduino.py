19e217ac430f1e041472be2df3d576b9d2bb3663
fclef978 <fclef978@icloud.com>
2018-02-07 15:20:23 +0900

    ArduinoのDocString

--- arduino.py
+++ arduino.py
@@ -1,7 +1,7 @@
-# Arduino通信クラス
-# TL 鈴木宏和
-# 2017-08-26
-# 2017-08-26
+"""
+Arduinoとの通信をするクラスです。
+:author: 鈴木宏和
+"""
 
 from serial import Serial
 from time import sleep, time
@@ -10,18 +10,26 @@ import os
 
 
 class ArduinoOpenError(Exception):
-    
+    """
+    Arduinoとの通信失敗時用の例外クラスです。
+    """
     def __str__(self):
         return ('Arduino Open Failed')
 
 
 class Arduino:
+    """
+    Arduinoとの通信を行うクラスです。
+    """
 
     def __init__(self):
-        self.reset_pin = IO(IO.RESET, IO.OUT)
+        """
+        コンストラクタです。ここで通信開始から通信確立まで行います。
+        """
+        self.__reset_pin = IO(IO.RESET, IO.OUT)
         # self.reset_pin.off()
         # sleep(0.01)
-        self.reset_pin.on()
+        self.__reset_pin.on()
         sleep(0.5)
         if os.name == 'nt':
             self.port = 'COM4'
@@ -29,86 +37,143 @@ class Arduino:
             self.port = '/dev/ttyACM0'
         self.baud_rate = 2000000
         try:
-            self.ser = Serial(self.port, self.baud_rate)
+            self.__ser = Serial(self.port, self.baud_rate)
         except:
-            self.ser = Serial('/dev/ttyACM1', self.baud_rate)
+            self.__ser = Serial('/dev/ttyACM1', self.baud_rate)
             self.port = '/dev/ttyACM1'
-        self.flush()
-        self.buf = []
+        self.__flush()
+        self.__buf = []
         print('Arduino Waiting...')
 
-        while self.open():
+        while self.__open():
             pass
 
         print("Arduino Opened on {0}".format(self.port))
 
     def __del__(self):
+        """
+        デストラクタです。ポートを閉じてArduinoをリセットします。
+        :return: なし
+        """
         try:
-            self.ser.close()
+            self.__ser.close()
         except TypeError:
             pass
-        self.reset_pin.off()
+        self.__reset_pin.off()
         sleep(1)
-        self.reset_pin.on()
+        self.__reset_pin.on()
 
-    def open(self):
-        self.write("RasPi:Ready;")
+    def __open(self):
+        """
+        Arduinoとの通信確立をするメソッドです。
+        :rtype: bool
+        :return: 成功したらTrue, 失敗したらFalse
+        """
+        self.__write("RasPi:Ready;")
         sleep(0.001)
-        response = self.read_str_until(';')
+        response = self.__read_str_until(';')
         if response == "Arduino:OK":
             return True
         return False
 
-    def flush(self):
-        self.ser.reset_input_buffer()
+    def __flush(self):
+        """
+        受信バッファを空にするメソッドです。
+        :return: なし
+        """
+        self.__ser.reset_input_buffer()
 
-    def available(self, num=0):
+    def __available(self, num=0):
+        """
+        受信バッファのデータ数を返します。
+        引数を指定すると、そのバイト分溜まっているか判定します。
+        :param int num: デフォルト引数で、指定すると、指定したバイト分バッファに溜まっているかの判定になる。
+        :rtype: int, bool
+        :return: バイト数または判定結果
+        """
         if num == 0:
-            return self.ser.in_waiting
+            return self.__ser.in_waiting
         elif num > 0:
-            return self.ser.in_waiting >= num
+            return self.__ser.in_waiting >= num
         else:
             return False
 
     def send(self, cmd_data):
+        """
+        Arduinoにコマンドを送信するメソッドです。
+        :param list cmd_data: コマンドです。
+        :return: なし
+        """
         ser_data = self.encode(cmd_data)
         print(ser_data)
-        self.write(ser_data)
+        self.__write(ser_data)
 
     def arduino_update(self):
+        """
+        Arduinoにコマンドアップデート信号を送るメソッドです。
+        :return: なし
+        """
         ser_data = self.encode(["update"])
-        self.write(ser_data)
+        self.__write(ser_data)
 
     def receive(self):
-        tmp = self.read_str_until(';')
+        """
+        Arduinoから情報を受信するメソッドです。
+        :rtype: list, bool
+        :return: 失敗するとFalseを、成功すると文字列の配列を返します。
+        """
+        tmp = self.__read_str_until(';')
         if not tmp:
             return False
         return tmp.split(':')
         
-    def read_one_byte(self):
-        result = self.ser.read()
+    def __read_one_byte(self):
+        """
+        バッファから1バイト読み出すメソッドです。
+        :rtype: bytearray
+        :return: 受信したものを返します。(長さ1のbytearrayでエンコーディングはASCII)
+        """
+        result = self.__ser.read()
         return result
 
-    def read_str_until(self, terminator):
+    def __read_str_until(self, terminator):
+        """
+        指定した文字が現れるまでバッファから読み出します。
+        途中で終わってしまった場合、その読みかけの文字列を内部で保存し、次回呼び出し時に先頭に連結します。
+        :param str terminator: 終了文字です(長さ1のstr)
+        :rtype: str
+        :return: 受信した文字列です。失敗すると空文字列になります。
+        """
         try:
-            while self.available(1):
-                tmp = self.read_one_byte().decode('utf-8')
+            while self.__available(1):
+                tmp = self.__read_one_byte().decode('utf-8')
                 if tmp == terminator:
-                    result = self.buf
-                    self.buf = []
+                    result = self.__buf
+                    self.__buf = []
                     break
-                self.buf.append(tmp)
+                self.__buf.append(tmp)
             else:
                 return False
             return ''.join(result)
         except UnicodeDecodeError:
             return ""
 
-    def write(self, ser_data):
-        self.ser.write(ser_data.encode('ASCII'))
+    def __write(self, ser_data):
+        """
+        Arduinoに文字列を送信するメソッドです。
+        :param str ser_data: 送信文字列です。
+        :return: なし
+        """
+        self.__ser.write(ser_data.encode('ASCII'))
 
     @staticmethod
     def encode(cmd_data):
+        """
+        コマンド型リストを送信型の文字列に変換します。
+        :param list cmd_data: コマンドです。
+        :rtype: str
+        :return: 送信用文字列です。
+        """
         cmd = cmd_data[0].lower()
         if cmd == "update":
             ser_data = "CU:1;"
